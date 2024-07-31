8d0b7cb2e0d53d328e4a79000f60598b47b220cc
jun <gmdwns9516@gmail.com>
2024-07-31 16:04:17 +0900

    first commit

---
+++ js/header.js
@@ -0,0 +1,33 @@
+// Search
+var search = $(".search");
+
+$("#search").on("click", function () {
+  search.slideDown(100, "linear", function () {});
+  $("body, .search_bg").addClass("active");
+});
+$("i").on("click", function () {
+  search.slideUp(100, "linear", function () {});
+  $("body, .search_bg").removeClass("active");
+});
+
+var sub = $("#sub");
+var item = $(".sub");
+
+$(".sub").hide();
+
+sub.mouseenter(function () {
+  $(this).children(".sub").stop().slideDown("fast");
+});
+sub.mouseleave(function () {
+  $(this).children(".sub").stop().slideUp("fast");
+});
+
+$(".bars").click(function () {
+  $(".gnb").animate({ left: 0 }, 300);
+  $("body, .gnb_bg").addClass("active");
+  $("html").animate({ scrollTop: 0 }, "fast");
+});
+$(".gnb_close").click(function () {
+  $(".gnb").animate({ left: -280 }, 300);
+  $("body, .gnb_bg").removeClass("active");
+});
